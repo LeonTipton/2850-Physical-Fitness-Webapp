5fbba07513cd417051d48d35268d4c4641e5e270
Leon Tipton <leonftipton@gmail.com>
2026-03-10 12:32:05 +0000

    refactor: realised that IdTable<Int> and IntIdTable are equivalent so reverted back to IntIdTable

--- database/src/BiometricsTable.kt
+++ database/src/BiometricsTable.kt
@@ -1,9 +1,9 @@
 package com.physicalfitness
 
 import org.jetbrains.exposed.v1.core.ReferenceOption
-import org.jetbrains.exposed.v1.core.dao.id.IdTable
+import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 
-object BiometricsTable : IdTable<Int>() {
+object BiometricsTable : IntIdTable() {
     override val id = reference("user_id", UserTable.id, ReferenceOption.CASCADE) // foreign key from UserTable
     val weight = float("weight")
     val height = float("height")
--- database/src/LoginTable.kt
+++ database/src/LoginTable.kt
@@ -1,9 +1,9 @@
 package com.physicalfitness
 
 import org.jetbrains.exposed.v1.core.ReferenceOption
-import org.jetbrains.exposed.v1.core.dao.id.IdTable
+import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 
-object LoginTable: IdTable<Int>() {
+object LoginTable: IntIdTable() {
     override val id = reference("user_id", UserTable, ReferenceOption.CASCADE) // foreign key from UserTable
     val username = varchar("username", MAX_VARCHAR_LEN)
     val email = varchar("email", MAX_VARCHAR_LEN)
