2d62773b8ddbc202bb0ad6dcd2081935c722b39f
Leon Tipton <leonftipton@gmail.com>
2026-03-10 11:53:49 +0000

    refactor: removed name for biometricstable

--- database/src/BiometricsTable.kt
+++ database/src/BiometricsTable.kt
@@ -3,7 +3,7 @@ package com.physicalfitness
 import org.jetbrains.exposed.v1.core.ReferenceOption
 import org.jetbrains.exposed.v1.core.Table
 
-object BiometricsTable : Table("biometrics") {
+object BiometricsTable : Table() {
     val uid = reference("user_id", UserTable, ReferenceOption.CASCADE) // foreign key from UserTable
     val weight = float("weight")
     val height = float("height")
