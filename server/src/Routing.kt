c8cbd3be3896f931bfcfd0f22ba4b3db1f4dde93
descendg <guoyy9588@gmail.com>
2026-03-18 06:18:20 +0800

    Update Routing.kt

--- server/src/Routing.kt
+++ server/src/Routing.kt
@@ -2,9 +2,24 @@ package com.physicalfitness
 
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
+import io.ktor.server.application.call
+import io.ktor.server.routing.get
+import io.ktor.server.response.respondText
 
 fun Application.configureRouting() {
     routing {
-        // add routes here
+
+         get("/") {
+            call.respondText("Home page")
+        }
+
+        get("/login") {
+            call.respondText("Login page")
+        }
+
+        get("/dashboard") {
+            call.respondText("Dashboard page")
+        }
+       
     }
 }
