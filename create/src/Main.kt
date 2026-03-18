84fe86e73d5c1a0a4f9db8a38d9d8378157133c0
Leon Tipton <leonftipton@gmail.com>
2026-03-18 14:56:13 +0000

    refactor: removed unnecessary variables and changed functions to remove returns as NameToIdMap is not needed

--- create/src/Main.kt
+++ create/src/Main.kt
@@ -1,15 +1,12 @@
 package com.physicalfitness
 
-import org.jetbrains.exposed.v1.core.StdOutSqlLogger
-import org.jetbrains.exposed.v1.core.dao.id.EntityID
-import org.jetbrains.exposed.v1.jdbc.SchemaUtils
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import java.io.FileReader
 import org.apache.commons.csv.CSVFormat
-import org.jetbrains.exposed.v1.core.and
-
+import org.jetbrains.exposed.v1.core.StdOutSqlLogger
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.core.innerJoin
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.insertAndGetId
 import org.jetbrains.exposed.v1.jdbc.select
@@ -20,8 +17,6 @@ const val GYMWORK_DATA = "csv/gymwork.csv"
 const val DISTANCE_DATA = "csv/distance.csv"
 const val SPORTS_DATA = "csv/sports.csv"
 const val WATERWORK_DATA = "csv/waterwork.csv"
-
-typealias NameToIdMap = LinkedHashMap<String, EntityID<Int>>
 
 fun main(args: Array<String>) {
     val sqlLogging = args.isNotEmpty() && args[0] == "--sql"
@@ -57,9 +52,9 @@ fun main(args: Array<String>) {
 
         addDistance(DISTANCE_DATA)
         addWaterwork(WATERWORK_DATA)
-        val exercises = addGymwork(GYMWORK_DATA)
-        val sports = addSports(SPORTS_DATA)
-         createRecommendations(exercises, sports)
+        addGymwork(GYMWORK_DATA)
+        addSports(SPORTS_DATA)
+         createRecommendations()
     }
 }
 
@@ -91,39 +86,32 @@ fun addWaterwork(filename: String) {
     }
 }
 
-fun addGymwork(filename : String) : NameToIdMap {
+fun addGymwork(filename : String) {
     FileReader(filename).use { reader ->
         val records = CSVFormat.DEFAULT.parse(reader).drop(1)
-        val exercises  = NameToIdMap()
         for (record in records) {
-            val exercieId = GymworkTable.insertAndGetId {
+            val exerciseId = GymworkTable.insertAndGetId {
                 it[exName] = record[0]
                 it[youtubeLink] = record[2]
             }
-            exercises[record[0]] = exercieId
-            val muscles = record[1].split(";").forEach { muscle ->
+            record[1].split(";").forEach { muscle ->
                 GymworkMusclesTable.insert {
-                    it[id] = exercieId
+                    it[id] = exerciseId
                     it[muscleName] = muscle
                 }
             }
         }
-        return exercises
     }
 }
 
-fun addSports(filename : String) : NameToIdMap {
+fun addSports(filename : String) {
     FileReader(filename).use { reader ->
         val records = CSVFormat.DEFAULT.parse(reader).drop(1)
-        val sports = NameToIdMap()
         for (record in records) {
             val sportId = SportsTable.insertAndGetId {
                 it[sportName] = record[0]
             }
-
-            sports[record[0]] = sportId
-
-            val muscles = record[1].split(";").forEach { muscle ->
+            record[1].split(";").forEach { muscle ->
                 SportsMusclesTable.insert {
                     it[id] = sportId
                     it[muscleName] = muscle
@@ -131,7 +119,7 @@ fun addSports(filename : String) : NameToIdMap {
                 }
             }
 
-            val secondary = record[2].split(";").forEach { muscle ->
+            record[2].split(";").forEach { muscle ->
                 SportsMusclesTable.insert {
                     it[id] = sportId
                     it[muscleName] = muscle
@@ -139,11 +127,10 @@ fun addSports(filename : String) : NameToIdMap {
                 }
             }
         }
-        return sports
     }
 }
 
-fun createRecommendations(exercises : NameToIdMap, sports : NameToIdMap) {
+fun createRecommendations() {
     val joinTable = GymworkMusclesTable.innerJoin(
             otherTable = SportsMusclesTable,
             onColumn = { GymworkMusclesTable.muscleName },
