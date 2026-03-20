1ca093803c73dda2a438437e92fb1356c2c12be6
descendg <xrrh4659@leeds.ac.uk>
2026-03-20 15:48:03 +0800

    Update Errors.kt

--- server/src/Errors.kt
+++ server/src/Errors.kt
@@ -5,11 +5,16 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.pebble.respondTemplate
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respondText
 
 fun Application.configureErrorHandling() {
     install(StatusPages) {
         status(HttpStatusCode.NotFound) { call, _ ->
             call.respondTemplate("needs page example") //HttpStatusCode.NotFound)
+        }
+        exception<Throwable> { call, cause ->
+            call.respondText("500: ${cause.message}")
+    
         }
     }
 }
