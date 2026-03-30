56f6a70458b4c59b1ce2400b26d14fb408dfda90
descendg <xrrh4659@leeds.ac.uk>
2026-03-30 23:11:01 +0800

    Update Routing.kt

--- server/src/Routing.kt
+++ server/src/Routing.kt
@@ -2,25 +2,28 @@ package com.physicalfitness
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.routing.routing
-import io.ktor.server.routing.get
+import io.ktor.server.http.content.staticFiles
 import io.ktor.server.response.respondFile
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import java.io.File
 
 fun Application.configureRouting() {
     routing {
+        staticFiles("/", File("server/src/frontend/front-end"))
+
+        staticFiles("/front-end", File("server/src/frontend/front-end"))
 
         get("/") {
-            call.respondFile(File("index.html"))
+            call.respondFile(File("server/src/frontend/front-end/index.html"))
         }
 
         get("/login") {
-            call.respondFile(File("pages/login_page.html"))
+            call.respondFile(File("server/src/frontend/front-end/pages/login_page.html"))
         }
 
         get("/register") {
-            call.respondFile(File("pages/register_page.html"))
+            call.respondFile(File("server/src/frontend/front-end/pages/register_page.html"))
         }
-
     }
 }
